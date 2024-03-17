3d07a7a78f6e5410a2e69dda978b7a4f19dbb186
David Lee <dav@davlee.com>
2024-03-17 11:51:02 +1100

    running preonic splanck (spleonic?) style for an ergo experiment todo: see what's missing and deal with it

--- config/preonic_matrix_keymap.h
+++ config/preonic_matrix_keymap.h
@@ -1,81 +1,69 @@
-/*                                      60 KEY MATRIX / LAYOUT MAPPING
-
+/*                                      60 KEY MATRIX SPLANCK LAYOUT 
   ╭────────────────────────┬────────────────────────╮ ╭─────────────────────────┬─────────────────────────╮
-  │  0   1   2   3   4   5 │  6   7   8   9  10  11 │ │ LX5 LX4 LX3 LX2 LX1 LX0 │ RX0 RX1 RX2 RX3 RX4 RX5 │
-  │ 12  13  14  15  16  17 │ 18  19  20  21  22  23 │ │ LT5 LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 RT5 │
-  │ 24  25  26  27  28  29 │ 30  31  32  33  34  35 │ │ LM5 LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 RM5 │
-  │ 36  37  38  39  40  41 │ 42  43  44  45  46  47 │ │ LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 │
-  │ 48  49  50  51  52  53 │ 54  55  56  57  58  59 │ │ LH5 LH4 LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 RH4 RH5 │
+  │  0   1   2   3   4   5 │  6   7   8   9  10  11 │ │ LX4 LX3 LX2 LX1 LX0 ___ │ ___ RX0 RX1 RX2 RX3 RX4 │
+  │ 12  13  14  15  16  17 │ 18  19  20  21  22  23 │ │ LT4 LT3 LT2 LT1 LT0 ___ │ ___ RT0 RT1 RT2 RT3 RT4 │
+  │ 24  25  26  27  28  29 │ 30  31  32  33  34  35 │ │ LM4 LM3 LM2 LM1 LM0 ___ │ ___ RM0 RM1 RM2 RM3 RM4 │
+  │ 36  37  38  39  40  41 │ 42  43  44  45  46  47 │ │ LB4 LB3 LB2 LB1 LB0 ___ │ ___ RB0 RB1 RB2 RB3 RB4 │
+  │ 48  49  50  51  52  53 │ 54  55  56  57  58  59 │ │ LH4 LH3 LH2 LH1 LH0 ___ │ ___ RH0 RH1 RH2 RH3 RH4 │
   ╰────────────────────────┴────────────────────────╯ ╰─────────────────────────┴─────────────────────────╯ */
-
 #pragma once
 
-#define LX0  5  // left-top row
-#define LX1  4
-#define LX2  3
-#define LX3  2
-#define LX4  1
-#define LX5  0
+#define LX0  4  // left-top row
+#define LX1  3
+#define LX2  2
+#define LX3  1
+#define LX4  0
 
-#define RX0  6  // right-top row
-#define RX1  7
-#define RX2  8
-#define RX3  9
-#define RX4 10
-#define RX5 11
+#define RX0  7  // right-top row
+#define RX1  8
+#define RX2  9
+#define RX3 10
+#define RX4 11
 
-#define LT0 17
-#define LT1 16
-#define LT2 15
-#define LT3 14
-#define LT4 13
-#define LT5 12
+#define LT0 16
+#define LT1 15
+#define LT2 14
+#define LT3 13
+#define LT4 12
 
-#define RT0 18
-#define RT1 19
-#define RT2 20
-#define RT3 21
-#define RT4 22
-#define RT5 23
+#define RT0 19
+#define RT1 20
+#define RT2 21
+#define RT3 22
+#define RT4 23
 
-#define LM0 29 
-#define LM1 28 
-#define LM2 27 
-#define LM3 26 
-#define LM4 25 
-#define LM5 24 
+#define LM0 28 
+#define LM1 27 
+#define LM2 26 
+#define LM3 25 
+#define LM4 24 
 
-#define RM0 30 
-#define RM1 31 
-#define RM2 32 
-#define RM3 33 
-#define RM4 34 
-#define RM5 35 
+#define RM0 31 
+#define RM1 32 
+#define RM2 33 
+#define RM3 34 
+#define RM4 35 
 
-#define LB0 41 
-#define LB1 40 
-#define LB2 39 
-#define LB3 38 
-#define LB4 37 
-#define LB5 36 
+#define LB0 40 
+#define LB1 39 
+#define LB2 38 
+#define LB3 37 
+#define LB4 36 
 
-#define RB0 42 
-#define RB1 43 
-#define RB2 44 
-#define RB3 45 
-#define RB4 46 
-#define RB5 47 
+#define RB0 43 
+#define RB1 44 
+#define RB2 45 
+#define RB3 46 
+#define RB4 47 
 
-#define LH0 53
-#define LH1 52
-#define LH2 51
-#define LH3 50
-#define LH4 49
-#define LH5 48
+#define LH0 52
+#define LH1 51
+#define LH2 50
+#define LH3 49
+#define LH4 48
 
-#define RH0 54
-#define RH1 55
-#define RH2 56
-#define RH3 57
-#define RH4 58
-#define RH5 59
+#define RH0 55
+#define RH1 56
+#define RH2 57
+#define RH3 58
+#define RH4 59
