13116f91b937b5e92c0e3b6d0b9aa2266fd620ff
David Lee <dav@davlee.com>
2024-03-29 00:15:58 +1100

    minor tweaks and tidies

--- config/defines.h
+++ config/defines.h
@@ -16,14 +16,16 @@
 #define TRAN_5 ___ ___ ___ ___ ___ 
 #define TRAN_3 ___ ___ ___  
 #define NONE_5 NONE NONE NONE NONE NONE 
+#define NONE_4 NONE NONE NONE NONE 
 #define NONE_3 NONE NONE NONE 
    
 // layers
 #define EXIT &to DEF
 #define TO_GAL &to GAL
 
-// for application shortcuts in Raycast
+// for custom shortcuts 
 #define MEH LC(LA(LSHFT))
+#define HYPER LC(LS(LG(LALT)))
 
 // L thumb
 #define CMD_TAB &mt LCMD TAB
@@ -81,6 +83,9 @@
 #undef COMBO_TERM 
 #define COMBO_TERM 15
 
+// FIXME !! 
+// make these work for Glove80
+//
 #define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4 
 #define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4 
 #define THUMBS LH3 LH2 LH1 RH1 RH2 RH3      
@@ -90,3 +95,10 @@
 #define BSWORD &kp LA(BSPC)
 #define ENT_MEH &mt MEH ENTER
 
+/* keycodes for Glove80 side keys, etc */
+
+#define MO_SYS &mo SYS
+#define PTR_ENT &lt PTR ENTER
+#define SQT_MEH &mt MEH SQT
+#define SLS_MEH &mt MEH SLASH
+#define BSL_HYP &mt HYPER BSLH
